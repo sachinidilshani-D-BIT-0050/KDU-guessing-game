b6faf6e09246c548916e62e163a502ab24038831
ishini2002 <wedageishini@gmail.com>
2025-05-03 03:43:56 +0530

    Added [Add timer to measure program time]

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,5 @@
+import time 
+start_time = time.time() 
 import random 
 number = random.randint(1, 100) 
 print("Guess a number between 1 and 10") 
@@ -6,3 +8,4 @@ if guess == number:
     print("You win!") 
 else: 
     print(f"Wrong! The number was {number}")
+print(f"Time taken: {time.time() - start_time:.2f}s") 
