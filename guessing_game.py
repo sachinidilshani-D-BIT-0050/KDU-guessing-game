a72d7d4381363ee08ea3afae9f443aabda49c23b
pinipaW <gimhanipinipa@gmail.com>
2025-05-03 00:57:08 +0530

    Add starter code

--- guessing_game.py
+++ guessing_game.py
@@ -2,7 +2,7 @@ import random
 number = random.randint(1, 10)
 print("Guess a number between 1 and 10")
 guess = int(input())
-if if guess < number:
+if guess < number:
     print("Too low!")
 elif guess > number:
-    print("Too high!"):
+    print("Too high!");
