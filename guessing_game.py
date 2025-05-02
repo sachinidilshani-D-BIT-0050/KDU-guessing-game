d9329e11a502274a4c3aa6e4f548e5dae3cc953b
pinipaW <gimhanipinipa@gmail.com>
2025-05-03 03:22:40 +0530

    Added [Fixed conflict using 1-75]

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import random 
-number = random.randint(1, 100) 
+number = random.randint(1, 75)
 print("Guess a number between 1 and 10") 
 guess = int(input())
 while True: 
@@ -9,21 +8,17 @@ while True:
     print("Play again? (y/n)") 
     if input().lower() != 'y': 
         break
-=======
-=======
->>>>>>> b0ecbbf0c81f292328090ab563ccaa5050c520eb
-<<<<<<< HEAD
+
+
 import random
-number = random.randint(1, 50)
+number = random.randint(1, 75)
 print("Guess a number between 1 and 10")
 guess = int(input())
 if guess < number:
  print("Too low!")
 elif guess > number:
  print("Too high!")
-=======
-=======
->>>>>>> e03cd43f9870f9877c131805402ac6cd80b52da6
+
 import random 
 number = random.randint(1, 100) 
 print("Guess a number between 1 and 10") 
