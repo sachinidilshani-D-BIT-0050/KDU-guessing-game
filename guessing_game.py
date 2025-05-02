e404a066f59540be35348360b67e35b65f349acf
UvindiTR <rathnayakeuvindi@gmail.com>
2025-05-03 01:07:54 +0530

    Added [Replay game]

---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+import random 
+number = random.randint(1, 10) 
+print("Guess a number between 1 and 10") 
+guess = int(input()) 
+while True: 
+    # (paste existing code here) 
+    print("Play again? (y/n)") 
+    if input().lower() != 'y': 
+        break 
